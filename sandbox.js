5b9d13c2065a8eed1736651011cbb62c6e01d8df
janakraman <hifzhana@gmail.com>
2021-03-07 20:15:50 +0700

    lesson-3 done

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = "mario";
+var age = 30;
+var isBlackBelt = false;
+character = "luigi";
+age = 12;
+isBlackBelt = true;
+var circumference = function (diameter) {
+    return Math.PI * diameter;
+};
+console.log(circumference(12));
---
+++ sandbox.ts
@@ -0,0 +1,14 @@
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
+
+character = "luigi";
+age = 12;
+isBlackBelt = true;
+
+const circumference = (diameter: number) => {
+  return Math.PI * diameter;
+};
+
+console.log(circumference(12));
+
